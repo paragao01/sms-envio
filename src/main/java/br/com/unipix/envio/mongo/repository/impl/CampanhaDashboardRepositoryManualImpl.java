20827312864e6f8f8cc9c1be2f1f12110b58f0ab
galotinho <galotinhodelta@gmail.com>
2022-01-22 01:49:29 -0300

    ajuste

--- src/main/java/br/com/unipix/envio/mongo/repository/impl/CampanhaDashboardRepositoryManualImpl.java
+++ src/main/java/br/com/unipix/envio/mongo/repository/impl/CampanhaDashboardRepositoryManualImpl.java
@@ -1,6 +1,8 @@
 package br.com.unipix.envio.mongo.repository.impl;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Repository;
 
 import br.com.unipix.envio.enumeration.StatusCampanhaEnum;
 import br.com.unipix.envio.enumeration.StatusProcessoEnum;
+import br.com.unipix.envio.mongo.model.CampanhaAgendada;
 import br.com.unipix.envio.mongo.model.CampanhaDashboard;
 import br.com.unipix.envio.mongo.repository.CampanhaDashboardRepositoryManual;
 
@@ -23,9 +26,21 @@ public class CampanhaDashboardRepositoryManualImpl implements CampanhaDashboardR
 	
 	@Override
 	public void updateStatusAgendado(LocalDateTime data, StatusProcessoEnum status, String id) {
-		Query query = new Query(new Criteria("agendamentos.data").lte(data).and("_id").is(id).and("agendamentos.status").ne("Processado"));
-		Update update = Update.update("agendamentos.$.status", status.getName());
-		mongoTemplate.updateMulti(query, update, CampanhaDashboard.class);
+		Query query = new Query(new Criteria("agendamentos.data").lte(data).and("_id").is(id));
+		CampanhaDashboard campanha = mongoTemplate.findOne(query, CampanhaDashboard.class);
+		List<CampanhaAgendada> agendamentos = new ArrayList<>();
+		for(CampanhaAgendada agendamento : campanha.getAgendamentos()) {
+			CampanhaAgendada a = agendamento;
+			if(agendamento.getData().compareTo(data)<=0 && !agendamento.getStatus().equals(StatusProcessoEnum.PROCESSADO.getName())){
+				a.setStatus(status.getName());
+			}
+			agendamentos.add(a);
+		}
+		
+		campanha.setAgendamentos(agendamentos);
+	
+		mongoTemplate.save(campanha);
+		
 	}
 
 
